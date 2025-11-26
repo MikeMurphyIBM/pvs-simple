2e9f75687e13c2f86e0a7d96634bdd0abf9fb14e
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-26 07:43:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,36 +11,25 @@ provider "ibm" {
   region = "us-south"
 }
 
-# PowerVS workspace
+# PowerVS workspace lookup
 data "ibm_resource_instance" "pvs_workspace" {
   name = "murphy"
 }
 
-# List ALL images in the workspace (CORRECT NAME)
-data "ibm_pi_images" "all_images" {
-  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
-}
-
-# Pick EXACT AIX image
-locals {
-  aix_image = [
-    for img in data.ibm_pi_images.all_images.images :
-    img if img.name == "AIX 7200-05-10"
-  ][0]
-}
-
-# Network lookup (old naming)
+# Network lookup (Schematics legacy API)
 data "ibm_pi_network" "pvs_network" {
   pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
   pi_network_name      = "murphy-subnet"
 }
 
-# Create VM
+# VM create (Schematics legacy API)
 resource "ibm_pi_instance" "my_power_vm" {
   pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
 
   pi_instance_name = "clone-test"
-  pi_image_id      = local.aix_image.id
+
+  # ✅ AIX image ID you provided
+  pi_image_id = "52f2891b-6e4b-4765-bc0e-43cdc036305a"
 
   memory       = 2
   processors   = 0.25
@@ -48,9 +37,10 @@ resource "ibm_pi_instance" "my_power_vm" {
 
   sys_type     = "s922"
   storage_type = "tier3"
+
   key_pair_name = "murphy-clone-key"
 
   pi_network {
-    network_id = data.ibm_pi_network.pvs_network.network_id
+    network_id = data.ibm_pi_network.pvs_network.id
   }
 }
