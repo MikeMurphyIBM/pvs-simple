f0bae8b75152a789e77f5e8f3a12eb7b9da41074
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-26 06:24:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,34 +16,34 @@ data "ibm_resource_instance" "pvs_workspace" {
   name = "murphy"
 }
 
-# Image
+# PowerVS Image (correct arguments for v1.85)
 data "ibm_pi_image" "os_image" {
-  cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
-  name              = "7200-05-10"
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
+  pi_image_name        = "7200-05-10"
 }
 
-# Subnet
+# PowerVS Network (correct arguments for v1.85)
 data "ibm_pi_network" "pvs_network" {
-  cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
-  name              = "murphy-subnet"
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
+  pi_network_name      = "murphy-subnet"
 }
 
-# PowerVS Instance (new format)
+# PowerVS Instance (correct arguments for v1.85)
 resource "ibm_pi_instance" "my_power_vm" {
-  cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.guid
 
   pi_instance_name = "clone-test"
-  pi_image_id      = data.ibm_pis_image.os_image.image_id
+  pi_image_id      = data.ibm_pi_image.os_image.pi_image_id
 
-  memory           = 2
-  processors       = 0.25
-  proc_type        = "shared"
+  memory      = 2
+  processors  = 0.25
+  proc_type   = "shared"
 
   pi_network {
-    network_id = data.ibm_pis_subnet.pvs_network.network_id
+    network_id = data.ibm_pi_network.pvs_network.network_id
   }
 
-  key_pair_name    = "clone"
-  sys_type         = "s922"
-  storage_type     = "tier3"
+  key_pair_name = "clone"
+  sys_type      = "s922"
+  storage_type  = "tier3"
 }
