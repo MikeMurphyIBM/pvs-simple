f3dced923dff0ea518026e14d90fd6acbaaa5151
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-26 10:29:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
-
-
+# Resource Group
 data "ibm_resource_group" "group" {
   name = "Default"
 }
 
-resource "ibm_resource_instance" "pvs_workspace" {
-  name              = var.pvs_workspace_name
-  service           = "power-iaas"
-  location          = var.dr_pvs_region
-  plan              = "power-virtual-server-group"
-  resource_group_id = data.ibm_resource_group.group.id
+# Existing PowerVS workspace
+data "ibm_resource_instance" "pvs_workspace" {
+  name = var.pvs_workspace_name
 }
 
+# Add SSH Key to PowerVS
 resource "ibm_pi_key" "key" {
-  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
   pi_key_name          = var.ssh_key_name
   pi_ssh_key           = var.ssh_key_rsa
 }
 
-
-
-resource "ibm_pi_image" "aix_image" {
-  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
-  pi_image_id          = var.pvs_aix_image_id
-  pi_image_name        = var.pvs_aix_image_name
-  timeouts {
-    create = "9m"
-  }
+# Get existing subnet
+data "ibm_pi_network" "pvs_network" {
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
+  pi_network_name      = "murphy-subnet"
 }
 
+# Create AIX LPAR
 resource "ibm_pi_instance" "clone" {
-    pi_memory             = var.pvs_dr_instance_memory
-    pi_processors         = var.pvs_dr_instance_cores
-    pi_instance_name      = var.pvs_dr_instance_name
-    pi_proc_type          = "shared"
-    pi_image_id           = ibm_pi_image.aix_image.image_id
-    pi_key_pair_name      = ibm_pi_key.key.pi_key_name
-    pi_sys_type           = "s922"
-    pi_cloud_instance_id  = ibm_resource_instance.pvs_workspace.guid
-    pi_pin_policy         = "none"
-    pi_health_status      = "WARNING"
-    pi_storage_type = "tier3"
+  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
+
+  pi_instance_name = var.pvs_dr_instance_name
+  pi_image_id      = var.pvs_aix_image_id    # we use your existing image ID
+
+  pi_memory     = var.pvs_dr_instance_memory
+  pi_processors = var.pvs_dr_instance_cores
+  pi_proc_type  = "shared"
+  pi_sys_type   = "s922"
+  pi_storage_type = "tier3"
+
+  pi_key_pair_name = ibm_pi_key.key.pi_key_name
+
+  pi_network {
+    network_id = data.ibm_pi_network.pvs_network.network_id
+  }
+
+  pi_pin_policy = "none"
 }
