447a40e660b628c1c748370c92e30552b9e8026e
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-26 09:33:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,63 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "ibm-cloud/ibm"
-      version = ">= 1.85.0"
-    }
+data "ibm_resource_group" "group" {
+  name = "Default"
+}
+
+resource "ibm_resource_instance" "pvs_workspace" {
+  name              = var.pvs_workspace_name
+  service           = "power-iaas"
+  location          = var.dr_pvs_region
+  plan              = "power-virtual-server-group"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+resource "ibm_pi_key" "key" {
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
+  pi_key_name          = var.ssh_key_name
+  pi_ssh_key           = var.ssh_key_rsa
+}
+
+resource "ibm_pi_network" "pvs_private_network" {
+  pi_network_name      = var.pvs_private_subnet_name
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
+  pi_network_type      = "vlan"
+  pi_cidr              = var.pvs_private_subnet_cidr
+  pi_dns               = ["127.0.0.1"]
+  pi_gateway           = var.pvs_private_subnet_gateway
+  pi_network_mtu       = var.pvs_private_subnet_mtu
+  pi_ipaddress_range {
+    pi_starting_ip_address  = var.pvs_private_subnet_start_address
+    pi_ending_ip_address    = var.pvs_private_subnet_end_address
   }
 }
 
-provider "ibm" {
-  region = "us-south"
+resource "ibm_pi_network" "pvs_public_network" {
+  pi_network_name      = var.pvs_public_subnet_name
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
+  pi_network_type      = "pub-vlan"
+  pi_network_mtu       = var.pvs_private_subnet_mtu
 }
 
-# 1. PowerVS Workspace Lookup (Finds the CRN of the service instance)
-data "ibm_resource_instance" "pvs_workspace" {
-  name = "murphy"
-}
-
-# 2. Network lookup
-data "ibm_pi_network" "pvs_network" {
-  # The CRN is required to associate the resource request with the workspace
-  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
-  
-  # Using pi_network_id avoids the deprecation warning previously encountered
-  pi_network_id        = "396e9198-d7e2-4860-97c4-9a44c146ec19"
-  
-  # CRITICAL FIX: Replaced unsupported 'pi_data_center' with the generic 'zone' 
-  # attribute and set the value to a specific PVS datacenter name.
-  zone                 = "dal10" # <-- REPLACE with your specific PVS Datacenter ID/Zone!
-}
-
-# 3. VM CREATE — PVS Instance Resource
-resource "ibm_pi_instance" "my_power_vm" {
-  # Link to the workspace ID (Cloud Resource Name)
-  pi_cloud_instance_id = data.ibm_resource_instance.pvs_workspace.id
-
-  # Mandatory Instance Information
-  pi_instance_name = "clone-test"
-  pi_image_id      = "52f2891b-6e4b-4765-bc0e-43cdc036305a"
-
-  # Compute/Sizing Attributes 
-  pi_memory       = 2
-  pi_processors   = 0.25
-  pi_proc_type    = "shared"
-
-  # System/Storage Attributes
-  pi_sys_type     = "s922"
-  pi_storage_type = "tier3"
-
-  pi_key_pair_name = "murphy-clone-key"
-
-  # Network Attachment
-  pi_network {
-    network_id = data.ibm_pi_network.pvs_network.id
+resource "ibm_pi_image" "aix_image" {
+  pi_cloud_instance_id = ibm_resource_instance.pvs_workspace.guid
+  pi_image_id          = var.pvs_aix_image_id
+  pi_image_name        = var.pvs_aix_image_name
+  timeouts {
+    create = "9m"
   }
+}
+
+resource "ibm_pi_instance" "dr_instance" {
+    pi_memory             = var.pvs_dr_instance_memory
+    pi_processors         = var.pvs_dr_instance_cores
+    pi_instance_name      = var.pvs_dr_instance_name
+    pi_proc_type          = "shared"
+    pi_image_id           = ibm_pi_image.aix_image.image_id
+    pi_key_pair_name      = ibm_pi_key.key.pi_key_name
+    pi_sys_type           = "s922"
+    pi_cloud_instance_id  = ibm_resource_instance.pvs_workspace.guid
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_network { network_id = ibm_pi_network.pvs_public_network.network_id }
+    pi_network { network_id = ibm_pi_network.pvs_private_network.network_id }
+    pi_storage_type = "tier1"
 }
