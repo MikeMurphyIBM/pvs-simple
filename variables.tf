e088116a243ec20d00cfec7cf12aa36a35212fdf
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-11-26 22:04:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,13 +27,13 @@ variable "pvs_workspace_name" {
 variable "pvs_aix_image_id" {
   description = "The image ID for the AIX Image we want to deploy"
   type = string
-  default = "5ccf25e5-e543-4fe7-bbc4-cf0fbb2be420"
+  default = "dbe4470c-03d3-4b19-b2c2-2084a21bb110"
 }
 
 variable "pvs_aix_image_name" {
   description = "The name of the image"
   type = string
-  default = "7200-05-10"
+  default = "IBMi-EMPTY"
 }
 
 variable "pvs_dr_instance_cores" {
